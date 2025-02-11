659ab448e5b3a8a3eda6e0302abd2a511c8af276
Hayk P <haykw@Hayks-iMac.local>
2025-02-10 18:21:52 -0800

    +t

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Michael Bennet",
+  name: "Michael Bennett",
   description: "Data Scientist.",
   navItems: [
     {
