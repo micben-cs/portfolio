7f90e0dd8295541b1a7494fffa078b1142208b12
Mike B <haykw@MB.local>
2025-04-27 23:52:37 -0700

    contact update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,40 +4,45 @@ import { Divider } from "@nextui-org/divider";
 
 export default function PricingPage() {
   return (
-    <Card className="mx-3 py-14 ">
-      <p className="text-lg font-bold text-default-600">Contact Information</p>
-      <Divider />
-      <div className="mt-4 space-y-2">
-        <p>
-          Country:
-          <span className="text-default-500"> United States</span>
-        </p>
-        <p>
-          State: <span className="text-default-500"> California</span>
-        </p>
-        <p>
-          City: <span className="text-default-500"> Los Angeles</span>
-        </p>
-        <p>
-          Zip Code: <span className="text-default-500">90027</span>
-        </p>
-        <p>
-          <a
-            className=" inline-flex gap-1"
-            href="mailto:	michaelbennett.cs@gmail.com"
-          >
-            Email:
-            <span className="text-default-500">
-              michaelbennett.cs@gmail.com
-            </span>
-          </a>
-        </p>
-        <p>
-          <a href="tel:+13236089665">
-            Phone: <span className="text-default-500"> (323) 608-9665</span>
-          </a>
-        </p>
-      </div>
-    </Card>
+    <div className="flex flex-col items-center">
+      <Card className="flex items-center mb-5 h-[215px] w-full">
+        <p className="w-full text-left text-default-300 p-5">Location</p>
+        <p className="pb-1 text-xl pt-7">United States</p>
+        <Divider />
+        <p className="pt-1 text-default-500">Los Angeles, California, 90027</p>
+      </Card>
+
+      <Card className="p-0 mb-5 overflow-hidden  h-[215px] cursor-pointer text-left w-full">
+        <p className="text-default-300 p-5">Business Card </p>
+        <p className="pt-9 pl-10 text-xl">Michael Bennett</p>
+        <Divider />
+        <p className="pt-0.5 pl-10  text-default-500">Data Scientist</p>
+      </Card>
+      <Card className="p-0 overflow-hidden  h-[215px] cursor-pointer text-left w-full">
+        <a href="mailto:	michaelbennett.cs@gmail.com">
+          <p className="pt-5 pl-6 pb-1 text-default-500 ">
+            michaelbennett.cs@gmail.com
+          </p>
+        </a>
+        <Divider />
+        <div className="flex items-center justify-between ">
+          <div>
+            <p className="pt-5 pl-6  text-default-500">michaelbennett.dev</p>
+            <p className="pt-5 pl-6 pb-7 text-[12px] text-default-500">
+              Scan the QR code to visit my portfolio
+            </p>
+          </div>
+          <img
+            src="/b_card/qr-code.svg"
+            alt="QR Code"
+            className="w-[108px] pr-5"
+          />
+        </div>
+        <Divider />
+        <a href="tel:+13236089665">
+          <p className="pt-2 pl-6  text-default-500">(323) 608-9665</p>
+        </a>
+      </Card>
+    </div>
   );
 }
